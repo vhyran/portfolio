8771486ec2cbfa8869e0e0b71dad1a3363a5fa4e
xarcgit <yasouo@proton.me>
2025-01-01 18:38:11 +0530

    feat: replace Headless UI components with Radix UI themes, update App structure, and remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import Navbar from './components/Navbar';
+import { Button, Flex, Text } from '@radix-ui/themes';
 
 function Main() {
 	return (
 		<>
-			<Navbar />
-			<div className="text-text fake-content p-6 pt-20">
-				<h1 className="text-2xl font-bold mb-4">Welcome to the Page</h1>
-				<p className="mb-4 ">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et arcu
-					vitae turpis malesuada elementum. Praesent auctor, nulla vel dictum
-					ultricies, nisi magna sollicitudin purus, sed sagittis eros magna nec
-					augue. Proin vehicula libero nec metus efficitur, at dignissim velit
-					sollicitudin.
-				</p>
-				{[...Array(200)].map((_, index) => {
-					const uniqueKey = `item-${index}-${Math.random().toString(36).substr(2, 9)}`;
-					return (
-						<p key={uniqueKey} className="mb-4">
-							This is some placeholder content for scrolling. Item {index + 1}.
-						</p>
-					);
-				})}
-			</div>
+			<Flex direction="column" gap="2">
+				<Text>Hello from Radix Themes :)</Text>
+				<Button>Let's go</Button>
+			</Flex>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import '@radix-ui/themes/styles.css';
+import 'index.css';
 import App from './App.tsx';
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<Theme accentColor="amber" grayColor="sand">
+			<App />
+			<ThemePanel />
+		</Theme>
 	</StrictMode>,
 );
