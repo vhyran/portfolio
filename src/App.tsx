d95b27bc2b77e16069c6966b394f38fc9c5bd56c
xarcgit <yasouo@proton.me>
2025-01-01 22:10:06 +0530

    feat: add Navbar component to App and update layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from './components/Navbar';
+
 function Main() {
 	return (
 		<>
+			<Navbar />
 			<div className="container mx-auto p-4 text-text text-center">
 				<h1>Welcome to My Portfolio</h1>
 				<p>This is an example template for my portfolio website.</p>
